27fab9e62e1b721f1ce77bc0f8903cd50d5e3e35
Shota Tokunaga <s.toku.24@gmail.com>
2018-06-21 20:26:42 +0900

    arrange this repo

--- PedestrianTrackingFromMovingCameraExample/visiondemos.m
+++
@@ -1,7 +0,0 @@
-function visiondemos
-% VISIONDEMOS Index of Computer Vision System Toolbox examples.
-%   Display index of Computer Vision System Toolbox examples.
- 
-%   Copyright 2004 The MathWorks, Inc.
-
-demo('toolbox','computer vision')
--- talker_listener/listener.m
+++
@@ -1,6 +0,0 @@
-%rosinit 
-node = robotics.ros.Node('listener');
-sub = robotics.ros.Subscriber(node,'chatter','std_msgs/String',@chattercallback);
-function chattercallback(~,msg)
-    disp(msg.Data);
-end
