4a87a1cfc808504544311e5413253eaa08241291
Shota Tokunaga <s.toku.24@gmail.com>
2018-06-26 12:12:56 +0900

    make downsampling filter

--- node/Autoware_node/PCD_Downsampling/gredAverage.m
+++ node/Autoware_node/PCD_Downsampling/gredAverage.m
@@ -12,6 +12,7 @@ sub = robotics.ros.Subscriber(node, '/points_raw', 'sensor_msgs/PointCloud2', @g
 function grid_downsampling_callback(~, msg)
 	ptCloud = pointCloud(readXYZ(msg));
 
+	% gridStep is voxelsize
 	gridStep = 0.1;
 	filtered_ptCloud = pcdownsample(ptCloud,'gridAverage',gridStep);
 
---
+++ node/Autoware_node/PCD_Downsampling/nonuniformGridSample.m
@@ -0,0 +1,20 @@
+%% Initialization
+clear; clc;
+rosshutdown;
+rosinit;
+
+obj.videoPlayer = vision.VideoPlayer('Position', [29, 597,643,386]);
+
+%% Define node
+node = robotics.ros.Node('grid_downsampling_matlab');
+sub = robotics.ros.Subscriber(node, '/points_raw', 'sensor_msgs/PointCloud2', @grid_downsampling_callback);
+
+function grid_downsampling_callback(~, msg)
+	ptCloud = pointCloud(readXYZ(msg));
+
+	% must set maxNumPoints >= 6
+	maxNumPoints = 10;
+	filtered_ptCloud = pcdownsample(ptCloud,'nonuniformGridSample',maxNumPoints);
+
+    pcshow(filtered_ptCloud);
+end
