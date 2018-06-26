c3007853b71814b23422f478a3ba39de4ad3174a
T-Shota <s.toku.24@gmail.com>
2018-06-26 10:53:41 +0900

    fix gredAverage.m

--- node/Autoware_node/PCD_Downsampling/gredAverage.m
+++ node/Autoware_node/PCD_Downsampling/gredAverage.m
@@ -1,4 +1,4 @@
-## Initialization
+%% Initialization
 clear; clc;
 rosshutdown;
 rosinit;
@@ -6,17 +6,17 @@ rosinit;
 global obj;
 obj.videoPlayer = vision.VideoPlayer('Position', [29, 597,643,386]);
 
-## Define node
+%% Define node
 node = robotics.ros.Node('grid_downsampling_matlab');
 sub = robotics.ros.Subscriber(node, '/points_raw', 'sensor_msgs/PointCloud2', @grid_downsampling_callback);
 
 function grid_downsampling_callback(~, msg)
 	global obj;
-	ptCloud = pcread(msg);
+	ptCloud = readXYZ(msg);
 
 	gridStep = 2;
 	filtered_ptCloud = pcdownsample(ptCloud,'gridAverage',gridStep);
 
-	step(obj.videoPlayer, iltered_ptCloud);
+	step(obj.videoPlayer, filtered_ptCloud);
 
 end
