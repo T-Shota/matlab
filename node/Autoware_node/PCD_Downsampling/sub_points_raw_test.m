924c031cc84d5aa1ac53793536702c0fb9253272
T-Shota <s.toku.24@gmail.com>
2018-07-12 17:58:45 +0900

    sample point_raw

--- node/Autoware_node/PCD_Downsampling/sub_points_raw_test.m
+++ node/Autoware_node/PCD_Downsampling/sub_points_raw_test.m
@@ -1,3 +1,20 @@
+%% Initialization
+clear all;
+clc;
+rosshutdown;
 rosinit;
-sub = rossubscriber('/points_raw');
-msg = receive(sub);
+global node;
+
+%% Define node
+node = robotics.ros.Node('pointCloud_test');
+sub = robotics.ros.Subscriber(node, '/points_raw', 'sensor_msgs/PointCloud2', @pointCloud_test_callback);
+
+
+function pointCloud_test_callback(~, msg)
+    global node;
+    ptMsg = msg;
+
+    plane_pub = robotics.ros.Publisher(node, '/filterd_points','sensor_msgs/PointCloud2');
+    send(plane_pub, ptMsg);
+
+end
